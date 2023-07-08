2ad88f0c42ab715aa751ff78f0090b9e99db95f0
Ritik Rajdev <ritikrajdev761@gmail.com>
2023-07-09 01:33:13 +0530

    fix: some optimizations and add page not found

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { CompoundDetailComponent } from './pages/compound-detail/compound-detail.component';
 import { AddCompoundComponent } from './pages/add-compound/add-compound.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/compounds', pathMatch: 'full' },
@@ -16,6 +17,12 @@ const routes: Routes = [
     path: 'compounds/:id',
     component: CompoundDetailComponent,
     title: 'Compound Detail',
+  },
+  {
+    path: '**',
+    pathMatch: 'full',
+    component: PageNotFoundComponent,
+    title: '404 - Page Not Found',
   },
 ];
 
