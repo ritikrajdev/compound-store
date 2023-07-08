59552573275f28eac3bc552b382fdabede57f543
Ritik Rajdev <ritikrajdev761@gmail.com>
2023-07-08 21:36:41 +0530

    feat: add message and page not found component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -9,9 +9,11 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './pages/home/home.component';
 import { AddCompoundComponent } from './pages/add-compound/add-compound.component';
 import { CompoundDetailComponent } from './pages/compound-detail/compound-detail.component';
+import { MessageComponent } from './components/message/message.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 
 @NgModule({
-  declarations: [AppComponent, CompoundsComponent, CompoundComponent, NavbarComponent, HomeComponent, AddCompoundComponent, CompoundDetailComponent],
+  declarations: [AppComponent, CompoundsComponent, CompoundComponent, NavbarComponent, HomeComponent, AddCompoundComponent, CompoundDetailComponent, MessageComponent, PageNotFoundComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
